96ddf871b790261041972a84a28e3dea9136ec0b
ettyan <eyama0032@gmail.com>
2021-01-11 19:37:38 +0900

    別のユーザーが投稿画面に遷移することを防ぐ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -52,7 +52,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    if user_signed_in? && current_user.id != @prototype.user_id
       redirect_to action: :index
     end
   end
